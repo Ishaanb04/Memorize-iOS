285243044323f0394d8215fd00195df98f0bbe88
Ishaan Bhasin <ibhasin@terpmail.umd.edu>
2023-02-10 02:39:06 -0500

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Ishaan Bhasin on 2/10/23.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
